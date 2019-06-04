1262efa7d9f1e56d8f599b64a5f81acf18369603
Merwin <Merwin@LAPTOP-I0C2ETIR>
2019-06-04 16:25:38 +0530

    BACKEND PROCESS STARTED

---
+++ TEAMCOLLBACKEND/src/main/java/mer/mano/TEAMCOLLBACKEND/App.java
@@ -0,0 +1,13 @@
+package mer.mano.TEAMCOLLBACKEND;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
